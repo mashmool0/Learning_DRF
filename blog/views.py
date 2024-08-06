3660288c3831ae9a3e15fb5e50704a348fec372f
Amir Mashayekhi <ap4games2017@gmail.com>
2024-08-06 04:08:18 +0330

    test serializers

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from .models import Article
 from rest_framework.response import Response
 from django.views.generic import TemplateView
-
+from .serializers import ArticleSerializer
 
 # Create your views here.
 
@@ -11,8 +11,8 @@ from django.views.generic import TemplateView
 class ListBlogsView(APIView):
     def get(self, request):
         instance = Article.objects.all()
-        articles = [item.title for item in instance]
-        return Response(articles)
+        ser = ArticleSerializer(instance=instance , many=True)
+        return Response(data=ser.data)
 
 
 def blog_view(request):
